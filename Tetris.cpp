b013c49d9150524719c97c9a0d572ff8b022fa21
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-01-10 12:52:56 +0330

    a

--- Tetris.cpp
+++ Tetris.cpp
@@ -319,7 +319,7 @@ void playGame(Game game, Game temp)
                 else
                     break;
             }
-            
+
             else if (command == KB_LeftArrow)
             {
                 if (checkLeft(temp, game.upcomingShapes[0]))
@@ -371,6 +371,7 @@ void playGame(Game game, Game temp)
             {
                 system("cls");
                 cout << "Pause Menu";
+                // flag = exit sequence
             }
             else
             {
